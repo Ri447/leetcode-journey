75511a14f3e3502c3ad77b1ad63bdbbd10d9874e
Risika Singh <150253637+Ri447@users.noreply.github.com>
2026-08-27 21:52:01 +0530

    Time: 0 ms (100.00%) | Memory: 20.6 MB (18.03%) - LeetSync

---
+++ 1-two-sum/two-sum.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        pM = {}
+        for i, n in enumerate(nums):
+            diff = target-n
+            if diff in pM:
+                return [pM[diff],i]
+            pM[n] = i
+        return
